c215070165b9cf31df24cfcae55c32c8bc3e6a48
RenukaNathjogi <renukasnathjogi29@gmail.com>
2023-01-07 14:37:00 +0530

    Ngo register page structured designed

---
+++ lib/pages/register_page_ngo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RegisterNgo extends StatelessWidget {
+  const RegisterNgo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     appBar: AppBar(title: Text("NGO"),),
+     );
+
+  }
+}
