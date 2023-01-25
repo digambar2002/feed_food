23ba3f2b5ecc445cfd1fd9341641326cf879a5ae
bhaveshp47 <patilbhavesh4777@gmail.com>
2023-01-25 16:27:26 +0530

    validation of forgot page part 1

--- lib/authenticate/forgot_password.dart
+++ lib/authenticate/forgot_password.dart
@@ -6,9 +6,24 @@ import 'package:feed_food/widgets/btn.dart';
 import 'package:feed_food/widgets/text_fields.dart';
 import 'package:flutter/material.dart';
 
-class ForgotPassword extends StatelessWidget {
+class ForgotPassword extends StatefulWidget {
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
   // Conroller
   var _email = TextEditingController();
+
+  final _ForgotFormKey = GlobalKey<FormState>();
+
+  authenticate(BuildContext context) {
+    if (_ForgotFormKey.currentState!.validate()) {
+      Navigator.pushNamed(context, FeedFoodRoutes().OtpPage);
+    } else {
+      print(_ForgotFormKey.currentState?.validate());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,65 +32,67 @@ class ForgotPassword extends StatelessWidget {
         physics: BouncingScrollPhysics(),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(children: [
-            SizedBox(
-              height: 10,
-            ),
-            Image.asset(
-              "assets/images/forgot_pass.png",
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              alignment: Alignment.center,
-              // forgotpasswordUYR (2:682)
+          child: Form(
+            key: _ForgotFormKey,
+            child: Column(children: [
+              SizedBox(
+                height: 10,
+              ),
+              Image.asset(
+                "assets/images/forgot_pass.png",
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                alignment: Alignment.center,
+                // forgotpasswordUYR (2:682)
 
-              child: Text(
-                'Forgot Password?',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 20,
-                horizontal: 15,
-              ),
-              child: Text(
-                "Don’t worry ! It happens. Please enter the Email id we will send the OTP on this Email.",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                  color: Colors.black54,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 10,
-              ),
-              child: FoodTextField().buildEmail(_email),
-            ),
-            SizedBox(height: 25),
-            SizedBox(
-              height: 50,
-              width: 320,
-              child: ElevatedButton(
-                onPressed: (() =>
-                    Navigator.pushNamed(context, FeedFoodRoutes().OtpPage)),
                 child: Text(
-                  "Continue",
-                  style: TextStyle(fontSize: 24),
+                  'Forgot Password?',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
                 ),
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.deepPurple[300],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    )),
               ),
-            ),
-          ]),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 15,
+                ),
+                child: Text(
+                  "Don’t worry ! It happens. Please enter the Email id we will send the OTP on this Email.",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                child: FoodTextField().buildEmail(_email),
+              ),
+              SizedBox(height: 25),
+              SizedBox(
+                height: 50,
+                width: 320,
+                child: ElevatedButton(
+                  onPressed: (() => authenticate(context)),
+                  child: Text(
+                    "Continue",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.deepPurple[300],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      )),
+                ),
+              ),
+            ]),
+          ),
         ),
       ),
     );
