8769c9820bbe603fde7b951af888f0070f6b5b72
bhaveshp47 <patilbhavesh4777@gmail.com>
2023-01-25 14:49:05 +0530

    mobile size

--- lib/authenticate/forgot_password.dart
+++ lib/authenticate/forgot_password.dart
@@ -15,67 +15,68 @@ class ForgotPassword extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
-        child: Column(children: [
-          SizedBox(
-            height: 10,
-          ),
-          Image.asset(
-            "assets/images/forgot_pass.png",
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            // forgotpasswordUYR (2:682)
-            margin: EdgeInsets.fromLTRB(0, 0, 230, 14),
-            constraints: BoxConstraints(
-              maxWidth: 140,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: [
+            SizedBox(
+              height: 10,
             ),
-            child: Text(
-              'Forgot\nPassword?',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+            Image.asset(
+              "assets/images/forgot_pass.png",
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 5,
-              horizontal: 30,
+            SizedBox(
+              height: 20,
             ),
-            child: Text(
-              "Don’t worry ! It happens. Please enter the Email id we will send the OTP on this Email.",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 15,
-                color: Colors.black54,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
-            ),
-            child: FoodTextField().buildEmail(_email),
-          ),
-          SizedBox(height: 25),
-          SizedBox(
-            height: 60,
-            width: 360,
-            child: ElevatedButton(
-              onPressed: (() =>
-                  Navigator.pushNamed(context, FeedFoodRoutes().OtpPage)),
+            Container(
+              alignment: Alignment.center,
+              // forgotpasswordUYR (2:682)
+
               child: Text(
-                "Continue",
-                style: TextStyle(fontSize: 24),
+                'Forgot Password?',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
               ),
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.deepPurple[300],
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  )),
             ),
-          ),
-        ]),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 20,
+                horizontal: 15,
+              ),
+              child: Text(
+                "Don’t worry ! It happens. Please enter the Email id we will send the OTP on this Email.",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Colors.black54,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              child: FoodTextField().buildEmail(_email),
+            ),
+            SizedBox(height: 25),
+            SizedBox(
+              height: 50,
+              width: 320,
+              child: ElevatedButton(
+                onPressed: (() =>
+                    Navigator.pushNamed(context, FeedFoodRoutes().OtpPage)),
+                child: Text(
+                  "Continue",
+                  style: TextStyle(fontSize: 24),
+                ),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.deepPurple[300],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+              ),
+            ),
+          ]),
+        ),
       ),
     );
   }
