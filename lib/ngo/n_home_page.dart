06de982053af340bdcf222777e9cc804f5264b3f
RenukaNathjogi <renukasnathjogi29@gmail.com>
2023-02-03 21:08:34 +0530

    Made some changes to  walkthrough.

--- lib/ngo/n_home_page.dart
+++ lib/ngo/n_home_page.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 class NHomePage extends StatefulWidget {
-
   const NHomePage({super.key});
 
   @override
   State<NHomePage> createState() => _NHomePageState();
 }
-
 
 class _NHomePageState extends State<NHomePage> {
   var _currentIndex = 0;
@@ -17,101 +15,85 @@ class _NHomePageState extends State<NHomePage> {
   Widget build(BuildContext context) {
     return Material(
         child: Scaffold(
-           bottomNavigationBar: SalomonBottomBar(
-      currentIndex: _currentIndex,
-      onTap: (i) => setState(() => _currentIndex = i),
-      items: [
-        /// Home
-        SalomonBottomBarItem(
-          icon: Icon(Icons.home_outlined),
-          title: Text("Home"),
-          selectedColor: Colors.purple,
-        ),
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _currentIndex,
+        onTap: (i) => setState(() => _currentIndex = i),
+        items: [
+          /// Home
+          SalomonBottomBarItem(
+            icon: Icon(Icons.home_outlined),
+            title: Text("Home"),
+            selectedColor: Colors.purple,
+          ),
 
-        SalomonBottomBarItem(
-          icon: Icon(Icons.notification_add_outlined),
-          title: Text("Notification"),
-          selectedColor: Colors.orange,
-        ),
+          SalomonBottomBarItem(
+            icon: Icon(Icons.notification_add_outlined),
+            title: Text("Notification"),
+            selectedColor: Colors.orange,
+          ),
 
-        /// Likes
-        SalomonBottomBarItem(
-          icon: Icon(Icons.add_box_outlined, size: 30),
-          title: Text("Add Request"),
-          selectedColor: Colors.red,
-        ),
+          /// Likes
+          SalomonBottomBarItem(
+            icon: Icon(Icons.add_box_outlined, size: 30),
+            title: Text("Add Request"),
+            selectedColor: Colors.red,
+          ),
 
-        /// Search
-        SalomonBottomBarItem(
-          icon: Icon(Icons.history),
-          title: Text("history"),
-          selectedColor: Colors.orange,
-        ),
+          /// Search
+          SalomonBottomBarItem(
+            icon: Icon(Icons.history),
+            title: Text("history"),
+            selectedColor: Colors.orange,
+          ),
 
-        /// Profile
-        SalomonBottomBarItem(
-          icon: Icon(Icons.person_2_outlined),
-          title: Text("Profile"),
-          selectedColor: Colors.teal,
-        ),
-      ],
-    ),
+          /// Profile
+          SalomonBottomBarItem(
+            icon: Icon(Icons.person_2_outlined),
+            title: Text("Profile"),
+            selectedColor: Colors.teal,
+          ),
+        ],
+      ),
 
-          body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 10,
-            ),
 
-            // Text for topleft corner
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                alignment: Alignment.topLeft,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Welcome",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 28,
-                      ),
+      // Text for topleft corner
+      body: SingleChildScrollView(
+        child: Container(
+          height: 300,
+          width: 600,
+          decoration: BoxDecoration(color: Color.fromARGB(255, 163, 119, 239)),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              alignment: Alignment.topLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
                     ),
-                    Text(
-                      "NGO Name",
-                      style: TextStyle(color: Colors.black, fontSize: 16),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/nhome.png"),
-                    fit: BoxFit.cover),
-              ),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 200, horizontal: 20),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 60,
-                      width: 360,
+                  ),
+                  Text(
+                    "NGO Name",
+                    style: TextStyle(color: Colors.white,
+                     fontSize: 24),
+                  ),
+                  Text(
+                  "Login to account",
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white),
                 ),
                 ],
-                ),
-                ),
-                ),
-                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     ),
-    ),
-    )
     );
   }
 }
